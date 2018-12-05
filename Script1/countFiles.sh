9f91a7cd1187a43d5075923ac211314a9702d11d
Ananye Agarwal <ananayagarwal@gmail.com>
2018-12-05 14:15:06 +0530

    Added comments to Script1 and debugged it

--- Script1/countFiles.sh
+++ Script1/countFiles.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# Counts the number of files in a given path with a given extension
+# bash countFiles.sh PATH [extension]
 
 if [[ $# -eq 0 ]] || [[ !(-d "$1") ]]
 	then 
+	echo "ERROR: Invalid PATH argument"
 	exit 1
 fi
 
 if [ $# -eq 1 ]
 	then
 	RESULT=0
-	ls -a "$1" | tail -n +3 | 
+	ls -a "$1" | tail -n +4 | 
 	{
 		while read file
 		do
